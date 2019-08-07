5812d70bfb94e6c8575345db25a5a7c66fa8188d
jarednapier <46578158+jarednapier@users.noreply.github.com>
2019-08-06 22:08:45 -0400

    .

---
+++ src/JS/CalculatorPad.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import '../CSS/CalculatorPad.css';
+import PropTypes from 'prop-types';
+
+const CalculatorPad = (props) => {
+
+};
+
+CalculatorPad.propTypes = {
+
+};
+
+export default CalculatorPad;
